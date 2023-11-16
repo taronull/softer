7618784acce3a14cb51322e7e6bbe5913c98bf43
Taro <taronull@gmail.com>
2023-11-16 19:20:44 +0900

    Add landing page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+  <a href="/">taro&amp;</a>
+  <nav>
+    <a href="/#work">Work</a>
+    <a href="mailto:taronull@gmail.com">Say hi</a>
+  </nav>
+</header>
+<section id="hero">
+  <h1>
+    Learning
+                Made
+    Transformational
+  </h1>
+</section>
+<section id="introduction">
+  <p>
+    taro& is a learning experience design studio. 
+    We create tailored learner journeys 
+    that add value to your brand 
+    and get your audience where they want to be.
+  </p>
+</section>
+<section id="invitation">
+    <p>
+      Hi! I'm Taro, your LXD
+      I believe learning is the key for a better future
+      People want to change for the better, would pay for it
+      But meaningful experience takes more than knowledge
+      That's why I do design processes for learning experience
+      It incorporates creativity, technology, and your uniqueness
+      Let's find out what we can do for your challenges
+      Feel free to reach out: taro@taroet.co
+      Connect with me on Substack, Twitter, and LinkedIn to stay updated
+    </p>
+</section>
+<section id="works">
+  <article>
+    <p>Creator online course</p>
+    <h1>Mind 101</h1>
+    <p>
+      Mind 101 bridges the gap between the mind 
+      and the brain to help meditators achieve their goals. 
+      Key challenge was to visualise abstract concepts 
+      and to facilitate the community.
+    </p>
+  </article>
+  <article>
+    <p>Customer education</p>
+    <h1>Notion Academy</h1>
+    <p>
+      Notion Academy has been a small part of Notion's help centre. 
+      This concept project explores what does it mean 
+      to provide the audience with a complete learner journey.
+    </p>
+  </article>
+  <article>
+    <p>Technical guide</p>
+    <h1>E-learning Specs</h1>
+    <p>
+      Modern e-learning has 3 data transmission standards with minute differences. 
+      This guide delineates each one from the others and puts them 
+      in a perspective to help users make a right choice.
+    </p>
+  </article>
+</section>
+<footer>
+  &copy; Taro 2023
+</footer>
