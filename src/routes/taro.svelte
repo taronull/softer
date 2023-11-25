32ef994a33b79aa8e68e9e537aa7f9a016a94c88
Taro <taronull@gmail.com>
2023-11-25 23:11:28 +0900

    Tweak styles

--- src/routes/taro.svelte
+++ src/routes/taro.svelte
@@ -1,28 +1,29 @@
 <script>
-  let bubbleStyle = "px-2 py-1 rounded lg:text-lg bg-slate-700/10"
-  let linkStyle = "underline decoration-from-font text-sky-600 hover:text"
+  let bubble = "px-2 py-1 rounded-lg lg:text-lg bg-slate-100 border border-slate-200"
+  let link = "underline decoration-from-font text-sky-600 hover:text"
 </script>
 
 <section class="grid justify-center gap-2 px-4 py-16">
   <img src="https://images.softer.design/taro.webp" alt="Taro" class="rounded-full w-14 sm:w-16 md:w-20 lg:w-24">
   <div class="grid gap-1 col-start-2 w-64 sm:w-72 md:w-80 lg:w-96">
     <p class="font-display font-semibold text-xl sm:text-2xl md:text-3xl lg-text-4xl">Taro</p>
-    <p class={bubbleStyle}>Hi! 👋 I'm Taro, your LX designer</p>
-    <p class={bubbleStyle}>Learning is the key for a better future</p>
-    <p class={bubbleStyle}>People want a change in their lives, would pay for it 🔥</p>
-    <p class={bubbleStyle}>But making a learning experience takes more than domain knowledge</p>
-    <p class={bubbleStyle}>And that's what we do! ✨</p>
-    <p class={bubbleStyle}>Our process combines creativity, technology, and your own uniqueness</p>
-    <p class={bubbleStyle}>Let's find out what we can do for your challenge 😉</p>
-    <p class={bubbleStyle}>
+    <p class={bubble}>Hi! 👋 I'm Taro, your LX designer</p>
+    <p class={bubble}>Learning is the key for a better future</p>
+    <p class={bubble}>People want a change in their lives, would pay for it 🔥</p>
+    <p class={bubble}>But making a learning experience takes more than domain knowledge</p>
+    <p class={bubble}>And that's what we do! ✨</p>
+    <p class={bubble}>Our process combines creativity, technology, and your own uniqueness</p>
+    <p class={bubble}>Let's find out what we can do for your challenge 😉</p>
+    <p class={bubble}>
       Feel free to reach out: 
-      <a href="mailto:taro@softer.design" class={linkStyle}>taro@softer.design</a>
+      <a href="mailto:taro@softer.design" class={link}>taro@softer.design</a>
     </p>
-    <p class={bubbleStyle}>
+    <p class={bubble}>
       Connect with me on 
-      <a href="https://taroand.substack.com" target="_blank" rel="noopener noreferrer" class={linkStyle}>Substack</a>, 
-      <a href="https://twitter.com/taronull" target="_blank" rel="noopener noreferrer" class={linkStyle}>Twitter</a>, and 
-      <a href="https://www.linkedin.com/in/taronull/" target="_blank" rel="noopener noreferrer" class={linkStyle}>LinkedIn</a>
+      <a href="https://taroand.substack.com" target="_blank" rel="noopener noreferrer" class={link}>Substack</a>, 
+      <a href="https://twitter.com/taronull" target="_blank" rel="noopener noreferrer" class={link}>Twitter</a>, 
+      <a href="https://www.instagram.com/_taronull/" target="_blank" rel="noopener noreferrer" class={link}>Instagram</a>, and 
+      <a href="https://www.linkedin.com/in/taronull/" target="_blank" rel="noopener noreferrer" class={link}>LinkedIn</a>
     </p>
   </div>
 </section>
