2e6728fcd776a49cdaeff4db8dce620a66d0b344
Taro <taronull@gmail.com>
2023-11-29 20:54:04 +0900

    Rename Value

---
+++ src/routes/value.svelte
@@ -0,0 +1,24 @@
+<section class="grid grid-cols-[min(100%,_40rem)] justify-center px-8 py-32">
+  <article class="grid gap-4">
+    <h1 class="w-64 font-display italic text-4xl">
+      Future learning hits different
+    </h1> 
+    <div class="grid gap-4 max-w-sm justify-self-end">
+      <p>
+        Softer makes learners ask for more.
+        They already know that learning is the way to make a real change.
+        Focusing on learning outcomes, we deliver learning experiences worth paying for. 
+      </p>
+      <ul class="list-disc list-inside">
+        <li>Creator online courses</li>
+        <li>Brand learning products</li>
+        <li>Software customer education</li>
+      </ul>
+      <p>
+        Softer sails with digital transformation and creator economy. 
+        It sets us apart from generic compliance training providers.
+        Every brand and personality makes a unique learning opportunity.
+      </p>
+    </div>
+  </article>
+</section>
