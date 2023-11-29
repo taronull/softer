8c9f903868858b445fccf9da1c0361b495c8c663
Taro <taronull@gmail.com>
2023-11-29 23:07:42 +0900

    Add Hotjar

---
+++ src/lib/hotjar.js
@@ -0,0 +1,6 @@
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3763493;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import "@fontsource-variable/fraunces/full-italic.css";
   import "@fontsource-variable/inter";
   import "../app.css";
+  import "$lib/hotjar"
   import Header from "./header.svelte"
   import Navigation from "./navigation.svelte"
   import Footer from "./footer.svelte"
