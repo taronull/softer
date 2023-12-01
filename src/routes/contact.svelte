e310b63b403c01b7180911356bde93e459f054b5
Taro <taronull@gmail.com>
2023-12-01 11:23:21 +0900

    Make Tally load

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,16 +1,12 @@
-<svelte:head>
-  <script src="https://tally.so/widgets/embed.js"></script>
-  <script>
-    Tally.loadEmbeds();
-  </script>
-</svelte:head>
+
   
 <section id="contact" class="grid grid-cols-[min(100%,_40rem)] justify-center justify-items-stretch gap-4 px-4">
   <h1 class="font-display text-3xl">Something in mind? <span class="italic">Get in touch.</span></h1>
   <iframe 
     data-tally-src="https://tally.so/embed/3jlJyE?alignLeft=1&hideTitle=1&dynamicHeight=1&transparentBackground=1" 
-    loading="lazy" 
     title="Softer Contact"
-
   ></iframe>
+  <script>
+    var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
+  </script>
 </section>
--- src/routes/showcase.svelte
+++ src/routes/showcase.svelte
@@ -3,6 +3,5 @@
     title="Softer showcase from Milanote"
     src="https://app.milanote.com/1R8B3p10ZWAU8M?p=qXpQRiY61SD" 
     class="h-[80svh]"
-    frameborder=”0″
   ></iframe>
 </section>
