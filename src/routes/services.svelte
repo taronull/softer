2f44457ac20351dc5052b4032a267f7a035ea545
Taro <taronull@gmail.com>
2023-12-01 10:21:34 +0900

    Make Services responsive

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -1,6 +1,6 @@
-<section class="grid justify-center">
-  <div class="grid grid-cols-[auto_auto] gap-8 text-6xl tracking-tighter">
-    <div class="self-start sticky top-1/4">Softer</div>
+<section class="grid justify-center p-4">
+  <div class="grid md:grid-cols-[auto_auto] md:gap-8 text-[10vw]/none md:text-6xl tracking-tighter">
+    <div class="self-start md:sticky md:top-1/4">Softer</div>
     <ul>
       <li>Learning Experience</li>
       <li>Creative Process</li>
@@ -12,7 +12,7 @@
       <li>Curriculum Mapping</li>
       <li>Learner Journey</li>
       <li>Learner Interaction</li>
-      <li>Learning Assessment</li>
+      <li>Learner Assessment</li>
       <li>Tools & Platforms</li>
       <li>Guide Materials</li>
     </ul>
