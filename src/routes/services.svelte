017bb7077d6c91570ab9ad2440c42ec5586362f2
Taro <taronull@gmail.com>
2023-12-02 17:42:08 +0900

    Add Services and network protocols

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -3,18 +3,21 @@
     <div class="self-start md:sticky md:top-1/4">Softer</div>
     <ul>
       <li>Learning Experience</li>
-      <li>Creative Process</li>
+      <li>Brand Narrative</li>
       <li>Storytelling</li>
-      <li>Key Visuals</li>
       <li>Learner Analysis</li>
       <li>Learner Persona</li>
       <li>Empathy Mapping</li>
-      <li>Curriculum Mapping</li>
       <li>Learner Journey</li>
-      <li>Learner Interaction</li>
+      <li>Curriculum Mapping</li>
+      <li>Creative Process</li>
+      <li>Key Visuals</li>
+      <li>Character Design</li>
+      <li>Infographics</li>
+      <li>User Interface</li>
+      <li>Guide Materials</li>
       <li>Learner Assessment</li>
       <li>Tools & Platforms</li>
-      <li>Guide Materials</li>
     </ul>
   </div>
 </section>
