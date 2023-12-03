1c6528611607e3eaad7dbe009893532793af0f7c
Taro <taronull@gmail.com>
2023-12-03 16:38:44 +0900

    Update Contact style

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,10 +1,11 @@
 
   
 <section id="contact" class="grid grid-cols-[min(100%,_40rem)] justify-center justify-items-stretch gap-4 px-4">
-  <h1 class="font-display text-3xl">What's your story? <span class="italic">Get in touch.</span></h1>
+  <h1 class="flex flex-wrap gap-x-2 font-display text-3xl">What's your story? <span class="italic">Get in touch.</span></h1>
   <iframe 
     data-tally-src="https://tally.so/embed/3jlJyE?alignLeft=1&hideTitle=1&dynamicHeight=1&transparentBackground=1" 
     title="Softer Contact"
+    class="min-h-[276px]"
   ></iframe>
   <script>
     var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
