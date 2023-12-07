61b1948f8f3c6973e6ffc5af457b6f6e99c30649
Taro <taronull@gmail.com>
2023-12-07 22:31:20 +0900

    Increase heading size

--- src/routes/whom.svelte
+++ src/routes/whom.svelte
@@ -1,8 +1,8 @@
 <section class="grid grid-cols-[min(100%,_32rem)] justify-center gap-16 px-4">
   <article class="grid gap-2">
     <div>
-      <p class="font-display italic font-thin text-5xl">@</p>
-      <h1 class="font-display text-3xl pl-8">Creators</h1>
+      <div class="font-display italic font-thin text-5xl">@</div>
+      <h1 class="font-display text-4xl pl-8">Creators</h1>
     </div>
     <ul class="list-['–_'] list-inside pl-8">
       <li>Online Courses</li>
@@ -12,8 +12,8 @@
   </article>
   <article class="grid gap-2 justify-self-end">
     <div>
-      <p class="font-display italic font-thin text-5xl">&</p>
-      <h1 class="font-display text-3xl pl-8">Companies</h1>
+      <div class="font-display italic font-thin text-5xl">&</div>
+      <h1 class="font-display text-4xl pl-8">Companies</h1>
     </div>
     <ul class="list-['–_'] list-inside pl-8">
       <li>Learning Products</li>
@@ -23,8 +23,8 @@
   </article>
   <article class="grid gap-2">
     <div>
-      <p class="font-display italic font-thin text-5xl">%</p>
-      <h1 class="font-display text-3xl pl-8">Softwares</h1>
+      <div class="font-display italic font-thin text-5xl">%</div>
+      <h1 class="font-display text-4xl pl-8">Softwares</h1>
     </div>
     <ul class="list-['–_'] list-inside pl-8">
       <li>Customer Education</li>
