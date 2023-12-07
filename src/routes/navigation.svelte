743c5186c6b932b06554bbc29b4674ff6c615851
Taro <taronull@gmail.com>
2023-12-07 15:09:06 +0900

    Change stuttering animations

--- src/routes/navigation.svelte
+++ src/routes/navigation.svelte
@@ -19,7 +19,7 @@
 </script>
 
 {#if visible}
-  <div transition:fade class="fixed left-0 bottom-[5svh] right-0 mx-auto w-fit flex justify-center">
+  <div transition:fade class="fixed left-0 bottom-[5svh] right-0 mx-auto w-fit flex justify-center z-10">
     <nav class="
       flex gap-4 font-medium text-sm md:text-base w-fit px-4 py-2 rounded-lg
       bg-white/50 border-t border-white/25 shadow backdrop-blur
