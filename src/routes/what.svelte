f577ec7ff377203a2b21bdb8d37544e658206454
Taro <taronull@gmail.com>
2023-12-07 14:42:34 +0900

    Add animation to What

--- src/routes/what.svelte
+++ src/routes/what.svelte
@@ -1,16 +1,44 @@
 <section id="hero" class="grid md:grid-cols-2 content-end min-h-[100svh] px-4 pt-24">
   <h1 class="
-    grid md:grid-rows-[subgrid] row-span-3 items-end
-    text-[18vw]/[0.9] font-display font-medium uppercase tracking-tight
+    grid md:grid-cols-[subgrid] col-start-1 col-span-2 md:grid-rows-[subgrid] row-start-1 row-span-3 
+    text-[18vw]/[0.9] font-display uppercase tracking-tight
   ">
     <span class="row-start-1">We</span>
-    <span class="row-start-2">change</span>
-    <span class="row-start-3">learning</span>
+    <span class="row-start-2 col-start-1 col-end-3">change</span>
+    <span class="row-start-3 col-start-1 col-end-3">learning</span>
   </h1>
-  <p class="justify-self-end max-w-[32ch] py-8 md:py-[1vw] md:col-start-2">
+  <p class="justify-self-end col-span-2 md:col-start-2 md:row-start-1 max-w-[32ch] py-8 md:py-[1vw]">
     Softer is a learning experience design studio.
     We create tailored learner journeys 
     to add value to your brand
     and get your audience where they want to be.
   </p>
 </section>
+
+<style>
+  h1 {
+    animation: grow 1.5s ease-out both;
+  }
+
+  p {
+    animation: fade 1.5s ease-out 1s both;
+  }
+
+  @keyframes grow {
+    from {
+      font-variation-settings: "wght" 100;
+    }
+    to {
+      font-variation-settings: "wght" 500;
+    }
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
