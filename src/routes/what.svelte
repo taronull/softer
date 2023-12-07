8b125321c7b4f292ec7fb744cdda622842936d0c
Taro <taronull@gmail.com>
2023-12-07 10:14:32 +0900

    Replace div with span

--- src/routes/what.svelte
+++ src/routes/what.svelte
@@ -3,9 +3,9 @@
     grid md:grid-rows-[subgrid] row-span-3 items-end
     text-[18vw]/[0.9] font-display font-medium uppercase tracking-tight
   ">
-    <div class="row-start-1">We</div>
-    <div class="row-start-2">change</div>
-    <div class="row-start-3">learning</div>
+    <span class="row-start-1">We</span>
+    <span class="row-start-2">change</span>
+    <span class="row-start-3">learning</span>
   </h1>
   <p class="justify-self-end max-w-[32ch] py-8 md:py-[1vw] md:col-start-2">
     Softer is a learning experience design studio.
